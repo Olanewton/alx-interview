f3d5c7005a93642069a8f04e02df6efaa379f2c7
Olanewton <Jolakanmi01@gmail.com>
2023-12-08 10:45:13 +0100

    0-prime_game.py updated

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 '''Prime Game'''
 
-
 def isWinner(x, nums):
     '''finds the winner'''
     winnerCounter = {'Maria': 0, 'Ben': 0}
@@ -18,7 +17,6 @@ def isWinner(x, nums):
     else:
         return None
 
-
 def isRoundWinner(n, x):
     '''find round winner'''
     list = [i for i in range(1, n + 1)]
@@ -31,7 +29,7 @@ def isRoundWinner(n, x):
         prime = -1
         for idx, num in enumerate(list):
             # if already picked prime num then
-            # find if num is multipl of the prime num
+            # find if num is multiple of the prime num
             if prime != -1:
                 if num % prime == 0:
                     selectedIdxs.append(idx)
